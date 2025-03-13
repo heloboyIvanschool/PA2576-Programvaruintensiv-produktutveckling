cf5c1f0dffb9cba675860f3eb0ccfa5412fb6f84
heloboyIvanschool <ivsv22@student.bth.se>
2025-03-13 18:56:30 +0100

    asd

--- website/auth.py
+++ website/auth.py
@@ -37,36 +37,38 @@ def sign_up():
     if request.method == 'POST':
         username = request.form.get('username')
         email = request.form.get('email')
-        favorite_genre = request.form.get('favorite_genre')
         password = request.form.get('password')
         password_confirm = request.form.get('password_confirm')
 
+        # Kontrollera att fälten inte är tomma
+        if not username or not email or not password or not password_confirm:
+            flash("All fields are required.", category="error")
+            return redirect(url_for("auth.sign_up"))
+
+        # Kontrollera om lösenord matchar
         if password != password_confirm:
             flash('Passwords do not match.', category='error')
             return redirect(url_for('auth.sign_up'))
 
-        # failchecks här
-        user = User.query.filter_by(email=email).first()
-        if user:
+        # Kontrollera om användarnamn eller e-post redan finns
+        user_by_email = User.query.filter_by(email=email).first()
+        user_by_username = User.query.filter_by(username=username).first()
+
+        if user_by_email:
             flash('Email already exists.', category='error')
+        elif user_by_username:
+            flash('Username already exists.', category='error')
         elif len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
         elif len(username) < 2:
-            flash('First name must be greater than 1 character.', category='error')
-
+            flash('Username must be greater than 1 character.', category='error')
         else:
-            new_user = User(username=username, email=email, favorite_genre=favorite_genre, password=generate_password_hash(password)) #profile_picture=profile_pic,
+            # Skapa ny användare om inga fel hittas
+            new_user = User(username=username, email=email, password=generate_password_hash(password))
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
             flash('Account created', category='success')
             return redirect(url_for('views.home'))
 
-        # user_exists = User.query.filter_by(username=username).first()
-        # email_exists = User.query.filter_by(email=email).first()
-
-        # if user_exists:
-        #     flash('Username already exists.', category='error') # sucsess
-        #     return redirect(url_for('register'))
-
-    return render_template('sign_up.html', user=current_user) #register
+    return render_template('sign_up.html', user=current_user)
--- website/models.py
+++ website/models.py
@@ -1,53 +1,73 @@
 from . import db
 from flask_login import UserMixin
-from sqlalchemy.sql import func
+from sqlalchemy.sql import func, validates
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
-    userId = db.Column(db.Integer, primary_key=True) # ifall man ändrar typen av nyckeln så msåte detta även ske i alla andra tabeller där nycklen används
+    user_id = db.Column(db.Integer, primary_key=True) # ifall man ändrar typen av nyckeln så msåte detta även ske i alla andra tabeller där nycklen används
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
-    profile_picture = db.Column(db.String, nullable=True)
-    favorite_genres = db.Column(db.PickleType, nullable=True)
-    createdAt = db.Column(db.DateTime(timezone=True), default=func.now())
-    role = db.Column(db.String(50), default='user')
+    created_at = db.Column(db.DateTime(timezone=True), default=func.now())
+    role = db.Column(db.String(10), default='user')
 
     #realations
     posts = db.relationship('Post', back_populates='user')
     likes = db.relationship('Like', back_populates='user')
     comments = db.relationship('Comment', back_populates='user')
 
+    def get_id(self):
+        return str(self.user_id)
+
     def __repr__(self):
         return f'<User {self.username}>'
 
 class Profiles(db.Model):
     __tablename__ = 'profiles'
-    profileId = db.Column(db.Integer, primary_key=True)
-    userId = db.Column(db.Integer, db.ForeignKey('users.userId', ondelete="CASCADE"), nullable=False)
+    profile_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id', ondelete="CASCADE"), unique=True, nullable=False)
     bio = db.Column(db.String(500), nullable=True)
+    profile_picture = db.Column(db.String, nullable=True)
+    favorite_genres = db.Column(db.JSON, nullable=True)
 
+    # Relationer
+    user = db.relationship("User", back_populates="profile")
 
 class Post(db.Model):
     __tablename__ = 'posts'
-    postId = db.Column(db.Integer, primary_key=True)
-    userId = db.Column(db.Integer, db.ForeignKey('users.userId'), nullable=False)
-    songId = db.Column(db.String, db.ForeignKey('songs.songId'), nullable=True)
+    post_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+
+    song_id = db.Column(db.String, db.ForeignKey('songs.song_id'), nullable=True)
+    album_id = db.Column(db.String, db.ForeignKey('albums.album_id'), nullable=True)
+    artist_id = db.Column(db.String, db.ForeignKey('artists.artist_id'), nullable=True)
+
     caption = db.Column(db.Text, nullable=True)
     likes = db.Column(db.Integer, default=0)
-    createdAt = db.Column(db.DateTime(timezone=True), default=func.now())
+    created_at = db.Column(db.DateTime(timezone=True), default=func.now())
 
-    #realations
+    # Relationer
     user = db.relationship('User', back_populates='posts')
-    likes = db.relationship('Like', back_populates='post', cascade="all, delete-orphan")
-    comments = db.relationship('Comment', back_populates='post', cascade="all, delete-orphan")
     song = db.relationship('Song', back_populates='posts')
+    album = db.relationship('Album', back_populates='posts')
+    artist = db.relationship('Artist', back_populates='posts')
+
+    @validates('song_id', 'album_id', 'artist_id')
+    def validate_only_one_content(self, key, value):
+
+        if value not in ['song', 'album', 'artist']:
+            raise ValueError("content_type must be 'song', 'album' or 'artist'")
+
+        content_count = sum(1 for content in [self.song_id, self.album_id, self.artist_id] if content is not None)
+        if content_count > 1:
+            raise ValueError("A post can only have one type of content: Song, Album, or Artist.")
+        return value
 
 class Like(db.Model):
     __tablename__ = 'likes'
-    likeId = db.Column(db.Integer, primary_key=True)
-    userId = db.Column(db.Integer, db.ForeignKey('users.userId'), nullable=False)
-    postId = db.Column(db.Integer, db.ForeignKey('posts.postId'), nullable=False)
+    like_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.post_id'), nullable=False)
 
     #relations
     user = db.relationship('User', back_populates='likes')
@@ -55,11 +75,11 @@ class Like(db.Model):
 
 class Comment(db.Model):
     __tablename__ = 'comments'
-    commentId = db.Column(db.Integer, primary_key=True)
-    userId = db.Column(db.Integer, db.ForeignKey('users.userId'), nullable=False)
-    postId = db.Column(db.Integer, db.ForeignKey('posts.postId'), nullable=False)
+    comment_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.post_id'), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    createdAt = db.Column(db.DateTime(timezone=True), default=func.now())
+    created_at = db.Column(db.DateTime(timezone=True), default=func.now())
 
     #realtions
     user = db.relationship('User', back_populates='comments')
@@ -67,12 +87,12 @@ class Comment(db.Model):
 
 class Song(db.Model):
     __tablename__ = 'songs'
-    songId = db.Column(db.String, primary_key=True)
+    song_id = db.Column(db.String, primary_key=True)
     title = db.Column(db.String, nullable=False)
     artist = db.Column(db.String, nullable=False)
     album = db.Column(db.String, nullable=True)
-    coverUrl = db.Column(db.String, nullable=True)
-    spotifyUrl = db.Column(db.String, nullable=False)
+    cover_url = db.Column(db.String, nullable=True)
+    spotify_url = db.Column(db.String, nullable=False)
 
     #relations
     posts = db.relationship('Post', back_populates='song', cascade="all, delete-orphan")
@@ -80,46 +100,35 @@ class Song(db.Model):
 class ProfileSong(db.Model):
     __tablename__ = 'profile_songs'
     id = db.Column(db.Integer, primary_key=True)  # Unikt ID
-    profileId = db.Column(db.Integer, db.ForeignKey('profiles.profileId', ondelete="CASCADE"), nullable=False)  # Användarens profil
-    songId = db.Column(db.String, db.ForeignKey('songs.songId', ondelete="CASCADE"), nullable=False)  # Länk till låten i `songs`
-
-user_songs = db.Table(
-    'user_songs',
-    db.Column('userId', db.Integer, db.ForeignKey('users.userId', ondelete="CASCADE"), primary_key=True),
-    db.Column('songId', db.String, db.ForeignKey('songs.songId', ondelete="CASCADE"), primary_key=True)
-)
+    profile_id = db.Column(db.Integer, db.ForeignKey('profiles.profile_id', ondelete="CASCADE"), nullable=False)  # Användarens profil
+    song_id = db.Column(db.String, db.ForeignKey('songs.song_id', ondelete="CASCADE"), nullable=False)  # Länk till låten i `songs`
 
 class Album(db.Model):
     __tablename__ = 'albums'
-    albumId = db.Column(db.String, primary_key=True)
+    album_id = db.Column(db.String, primary_key=True)
     title = db.Column(db.String, nullable=False)
     artist = db.Column(db.String, nullable=False)
-    coverUrl = db.Column(db.String, nullable=True)
-    spotifyUrl = db.Column(db.String, nullable=False)
+    cover_url = db.Column(db.String, nullable=True)
+    spotify_url = db.Column(db.String, nullable=False)
 
 class ProfileAlbum(db.Model):
     __tablename__ = 'profile_albums'
     id = db.Column(db.Integer, primary_key=True)  # Unikt ID
-    profileId = db.Column(db.Integer, db.ForeignKey('profiles.profileId', ondelete="CASCADE"), nullable=False)  # Användarens profil
-    profileId = db.Column(db.String, db.ForeignKey('songs.songId', ondelete="CASCADE"), nullable=False)
-
-user_albums = db.Table(
-    'profile_albums',
-    db.Column('userId', db.Integer, db.ForeignKey('users.userId', ondelete="CASCADE"), primary_key=True),
-    db.Column('albumId', db.String, db.ForeignKey('albums.albumId', ondelete="CASCADE"), primary_key=True)
-)
+    profile_id = db.Column(db.Integer, db.ForeignKey('profiles.profile_id', ondelete="CASCADE"), nullable=False)  # Användarens profil
+    profile_id = db.Column(db.String, db.ForeignKey('songs.song_id', ondelete="CASCADE"), nullable=False)
 
 class Artist(db.Model):
     __tablename__ = 'artists'
+    id = db.Column(db.Integer, primary_key=True)  # Unikt ID
     name = db.Column(db.String, nullable=False)
-    coverUrl = db.Column(db.String, nullable=True)
-    spotifyUrl = db.Column(db.String, nullable=False)
+    cover_url = db.Column(db.String, nullable=True)
+    spotify_url = db.Column(db.String, nullable=False)
 
 class ProfileArtist(db.Model):
     __tablename__ = 'profile_artists'
     id = db.Column(db.Integer, primary_key=True)  # Unikt ID
-    profileId = db.Column(db.Integer, db.ForeignKey('profiles.profileId', ondelete="CASCADE"), nullable=False)  # Användarens profil
-    artistId = db.Column(db.String, db.ForeignKey('songs.songId', ondelete="CASCADE"), nullable=False)
+    profile_id = db.Column(db.Integer, db.ForeignKey('profiles.profile_id', ondelete="CASCADE"), nullable=False)  # Användarens profil
+    album_id = db.Column(db.String, db.ForeignKey('songs.song_id', ondelete="CASCADE"), nullable=False)
 
 
 
@@ -129,22 +138,22 @@ class ProfileArtist(db.Model):
 #framtida implementationer
 def remove_favorite_song(user_id, song_id):
     # Ta bort låten från profile_songs-tabellen
-    db.session.query(ProfileSong).filter_by(profileId=user_id, songId=song_id).delete()
+    db.session.query(ProfileSong).filter_by(profile_id=user_id, song_id=song_id).delete()
     db.session.commit()
 
     # Kontrollera om låten fortfarande finns i profile_songs
-    song_still_used = db.session.query(ProfileSong).filter_by(songId=song_id).first()
+    song_still_used = db.session.query(ProfileSong).filter_by(song_id=song_id).first()
 
     if not song_still_used:
         # Om ingen längre har låten som favorit, ta bort den från songs-tabellen
-        db.session.query(Song).filter_by(songId=song_id).delete()
+        db.session.query(Song).filter_by(song_id=song_id).delete()
         db.session.commit()
 
 
     # following = db.relationship(
     #     'User', secondary=followers,
-    #     primaryjoin=(followers.c.followerId == userId),
-    #     secondaryjoin=(followers.c.followingId == userId),
+    #     primaryjoin=(followers.c.followerId == user_id),
+    #     secondaryjoin=(followers.c.followingId == user_id),
     #     backref=db.backref('followers', lazy='dynamic'),
     #     lazy='dynamic'
     # )
@@ -158,10 +167,10 @@ def remove_favorite_song(user_id, song_id):
     #         self.following.remove(user)
 
     # def is_following(self, user):
-    #     return self.following.filter(followers.c.followingId == user.userId).count() > 0
+    #     return self.following.filter(followers.c.followingId == user.user_id).count() > 0
 
 # followers = db.Table(
 #     'followers',
-#     db.Column('followerId', db.Integer, db.ForeignKey('users.userId', ondelete="CASCADE"), primary_key=True),
-#     db.Column('followingId', db.Integer, db.ForeignKey('users.userId', ondelete="CASCADE"), primary_key=True)
+#     db.Column('followerId', db.Integer, db.ForeignKey('users.user_id', ondelete="CASCADE"), primary_key=True),
+#     db.Column('followingId', db.Integer, db.ForeignKey('users.user_id', ondelete="CASCADE"), primary_key=True)
 # )
