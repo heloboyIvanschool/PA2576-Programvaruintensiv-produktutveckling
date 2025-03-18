a641cba4c38b456c7b9ecee6dd97fac4dcb3029b
heloboyIvanschool <ivsv22@student.bth.se>
2025-03-18 09:55:07 +0100

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS  # Importera CORS
 from os import path
 from flask_login import LoginManager
 from dotenv import load_dotenv
 import os
-from flask_cors import CORS
-# from . import db_events # at inte bort
 
 load_dotenv()
 
@@ -14,18 +13,18 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
 
+    # Flask-konfiguration
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 
     db.init_app(app)
 
-    CORS(app, supports_credentials=True)
+    # Tillåter React frontend att göra API-anrop till Flask
+    CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}}, supports_credentials=True)
 
+    # Importera och registrera Blueprints
     from .views import views
     from .auth import auth
-
-    from .models import User, OAuth
-
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
@@ -33,14 +32,14 @@ def create_app():
     app.register_blueprint(google_bp, url_prefix='/login')
     app.register_blueprint(oauth_routes, url_prefix='/')
 
+    from .models import User
     from . import db_events
-
 
     with app.app_context():
         db.drop_all()
         db.create_all()
-        # create_database()
 
+    # Flask-Login konfiguration
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
