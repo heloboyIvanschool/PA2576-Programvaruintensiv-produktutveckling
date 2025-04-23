1e888ab7cd5de2ee8d136e2c84dd8580e765812f
Emil11112 <emil.jbusiness@gmail.com>
2025-04-23 09:32:58 +0200

    Lade till kommentarer på button och discovery JS

--- website/React-Frontend/src/components/Discovery.js
+++ website/React-Frontend/src/components/Discovery.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import './Discovery.css';
 
+// Denna sidan är under utveckling..
 const Discovery = () => {
+  // Låter söktermen vara tom som initialt värde
   const [searchQuery, setSearchQuery] = useState('');
-
+// Vi skapar möjligheten att söka efter användare genom textinmatning
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
   };
@@ -13,7 +15,9 @@ const Discovery = () => {
     // Implement search functionality here
     console.log('Search query:', searchQuery);
   };
+  // Här under ska vi ha mer funktionalitet
 
+  // Renderar komponenten 
   return (
     <div className="discovery-container">
       <h1>Discover Users</h1>
